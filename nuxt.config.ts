e1ad8adfa040a08daafc47cda6e7924b13795e3b
swan14gg <owsbj1011@gmail.com>
2023-09-01 20:23:27 +0900

    introduce google fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,21 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  app: {
+    head: {
+      title: "swan14gg.dev",
+      link: [
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+JP:wght@400;700&family=M+PLUS+Rounded+1c:wght@400;700&display=swap",
+        },
+      ],
+    },
+  },
 });
