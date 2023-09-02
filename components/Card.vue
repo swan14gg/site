343a3543d55df25d874423f05e05467fc2a53337
swan14gg <owsbj1011@gmail.com>
2023-09-02 16:30:09 +0900

    attach client only to card font awesome icon

--- components/Card.vue
+++ components/Card.vue
@@ -11,10 +11,12 @@ defineProps<Props>();
 <template>
   <section :aria-labelledby="`${id}-heading`" class="p-5 bg-white rounded-xl">
     <header class="flex items-center mb-3">
-      <font-awesome-icon
-        :icon="`fa-solid fa-${icon}`"
-        class="w-4 md:w-5 me-3"
-      />
+      <ClientOnly>
+        <font-awesome-icon
+          :icon="`fa-solid fa-${icon}`"
+          class="w-4 md:w-5 me-3"
+        />
+      </ClientOnly>
       <h2 :id="`${id}-heading`" class="lg:text-lg font-bold">{{ title }}</h2>
     </header>
     <slot />
