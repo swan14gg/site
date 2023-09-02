58f28e15664eabff7a841aa925c58582e4bff8d7
swan14gg <owsbj1011@gmail.com>
2023-09-02 15:45:39 +0900

    fix vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,22 @@
-/// <reference types="vitest" />
-
-import { defineConfig } from "vite";
+import path from "path";
+import { defineConfig } from "vitest/config";
 import Vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
 
 export default defineConfig({
-  plugins: [Vue()],
+  plugins: [
+    Vue(),
+    AutoImport({
+      imports: ["vue"],
+    }),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"),
+    },
   },
 });
