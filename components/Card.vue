ad4d49bac3ad0d8f8e8698e249e928ecdd7874df
swan14gg <owsbj1011@gmail.com>
2023-09-02 16:12:46 +0900

    add aria labelledby

--- components/Card.vue
+++ components/Card.vue
@@ -9,13 +9,13 @@ defineProps<Props>();
 </script>
 
 <template>
-  <section :id="id" class="p-5 bg-white rounded-xl">
+  <section :aria-labelledby="`${id}-heading`" class="p-5 bg-white rounded-xl">
     <header class="flex items-center mb-3">
       <font-awesome-icon
         :icon="`fa-solid fa-${icon}`"
         class="w-4 md:w-5 me-3"
       />
-      <h2 class="lg:text-lg font-bold">{{ title }}</h2>
+      <h2 :id="`${id}-heading`" class="lg:text-lg font-bold">{{ title }}</h2>
     </header>
     <slot />
   </section>
