4bbdf0c609831e2e86cadcd827046c662af8a657
swan14gg <owsbj1011@gmail.com>
2023-11-23 15:54:54 +0900

    NuxtLayoutの位置変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,16 @@
 <template>
-  <NuxtLayout>
-    <div class="flex flex-col gap-4">
-      <SectionsAboutMe />
-      <div class="grid md:grid-cols-2 gap-4">
-        <SectionsApplications />
-        <SectionsGames />
-      </div>
-      <div class="grid md:grid-cols-2 gap-4">
-        <SectionsArticles />
-        <SectionsScraps />
-      </div>
-      <div class="grid md:grid-cols-2 gap-4">
-        <SectionsFavoriteArtists />
-      </div>
+  <div class="flex flex-col gap-4">
+    <SectionsAboutMe />
+    <div class="grid md:grid-cols-2 gap-4">
+      <SectionsApplications />
+      <SectionsGames />
     </div>
-  </NuxtLayout>
+    <div class="grid md:grid-cols-2 gap-4">
+      <SectionsArticles />
+      <SectionsScraps />
+    </div>
+    <div class="grid md:grid-cols-2 gap-4">
+      <SectionsFavoriteArtists />
+    </div>
+  </div>
 </template>
