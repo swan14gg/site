0e5b2c2a6856344f7814466b02a64426f9d1e9ee
swan14gg <owsbj1011@gmail.com>
2023-12-16 16:18:57 +0900

    Update: indexのカードレイアウトを縦1列に変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,8 @@
 <template>
-  <div class="flex flex-col gap-4">
+  <div class="space-y-4">
     <SectionsAboutMe />
-    <div class="grid md:grid-cols-2 gap-4">
-      <SectionsArticles />
-      <SectionsSlides />
-    </div>
-    <div class="grid md:grid-cols-2 gap-4">
-      <SectionsFavoriteArtists />
-    </div>
+    <SectionsArticles />
+    <SectionsSlides />
+    <SectionsFavoriteArtists />
   </div>
 </template>
