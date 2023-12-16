d1cf0d9617a6ee2cbada5c9ab9ab08531f8d8522
swan14gg <owsbj1011@gmail.com>
2023-12-16 23:01:15 +0900

    FIx: タイトルをh1からリンクに変更

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,7 +7,7 @@ const route = useRoute();
     <header
       class="p-3 lg:p-4 font-bold text-xl lg:text-2xl bg-white dark:bg-gray-700 flex justify-between"
     >
-      <h1>swan14gg.dev</h1>
+      <NuxtLink to="/">swan14gg.dev</NuxtLink>
       <ToggleTheme />
     </header>
     <main class="p-3 lg:p-4 max-w-4xl w-100 m-auto">
