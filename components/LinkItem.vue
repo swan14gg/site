94c25e3ebd330bc057d5e444a8fa0539c21b018a
swan14gg <owsbj1011@gmail.com>
2023-12-16 17:59:22 +0900

    Fix: リンクが横に伸びて当たり判定が広がっていたのを修正

--- components/LinkItem.vue
+++ components/LinkItem.vue
@@ -17,14 +17,14 @@ defineProps<LinkProps>();
 </script>
 
 <template>
-  <li
-    class="grid grid-cols-[20px_1fr_72px] sm:grid-cols-[20px_1fr_100px] gap-2 items-center p-2"
-  >
-    <span v-if="emoji !== undefined">{{ emoji }}</span>
-    <img v-else :src="iconUrl" :alt="title" width="20" height="20" />
-    <NuxtLink :to="pageUrl" class="link text-sm sm:text-base">
-      {{ title }}
-    </NuxtLink>
+  <li class="flex items-center p-2">
+    <div class="grid grid-cols-[20px_1fr] items-center gap-2">
+      <span v-if="emoji !== undefined">{{ emoji }}</span>
+      <img v-else :src="iconUrl" :alt="title" width="20" height="20" />
+      <NuxtLink :to="pageUrl" class="link text-sm sm:text-base">
+        {{ title }}
+      </NuxtLink>
+    </div>
     <div v-if="sub" class="ms-auto">
       <NuxtLink v-if="sub.link" :to="sub.link">
         <SubInfo :sub="sub" />
