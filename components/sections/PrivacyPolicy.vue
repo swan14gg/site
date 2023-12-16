931853e820bbfddf3985eba16cde97b867a8f238
swan14gg <owsbj1011@gmail.com>
2023-12-16 22:50:46 +0900

    Add: Privacy Policy

---
+++ components/sections/PrivacyPolicy.vue
@@ -0,0 +1,19 @@
+<template>
+  <Card id="privacy-policy" icon="user" title="Privacy Policy">
+    <div class="space-y-4">
+      <p>
+        当サイトでは、Googleが提供するアクセス解析ツール「Googleアナリティクス」を利用しています。Googleアナリティクスは、当サイトへの訪問者の行動を理解するためにCookieを使用してデータを収集します。収集されるデータには、訪問者のサイト利用状況、滞在時間、使用されたブラウザの種類、閲覧したページなどが含まれますが、これらの情報はすべて匿名で処理され、個人を特定することはありません。
+      </p>
+      <p>
+        また、Googleアナリティクスによるデータ収集は、お使いのブラウザでCookieを無効にすることにより拒否することが可能です。Cookieの無効化方法については、お使いのブラウザのヘルプセクションをご覧ください。Googleアナリティクスの使用に関する詳細は、<a
+          href="https://www.google.com/analytics/terms/jp.html"
+          class="link"
+          >Googleアナリティクス利用規約</a
+        >、および
+        <a href="https://policies.google.com/privacy?hl=ja" class="link"
+          >Googleのプライバシーポリシー</a
+        >を参照してください。
+      </p>
+    </div>
+  </Card>
+</template>
