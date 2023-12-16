48a6a0c7fe6ade1273f41f235df474e756143458
swan14gg <owsbj1011@gmail.com>
2023-12-16 22:57:33 +0900

    Add: privacy policy link

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup>
+const route = useRoute();
+</script>
+
 <template>
   <div class="bg-gray-100 dark:bg-gray-500 dark:text-white min-h-screen">
     <header
@@ -9,5 +13,13 @@
     <main class="p-3 lg:p-4 max-w-4xl w-100 m-auto">
       <slot />
     </main>
+    <footer class="text-center pb-4">
+      <NuxtLink
+        v-if="route.name !== 'privacy-policy'"
+        to="/privacy-policy"
+        class="link text-xs"
+        >プライバシーポリシー
+      </NuxtLink>
+    </footer>
   </div>
 </template>
